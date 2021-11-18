c1e075e48c0482c972da3d2c93cb92abb307ff2e
Kevin Torres <kevin_akd@hotmail.com>
2021-11-17 20:53:04 -0500

    form functional

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Formulario from "./components/Formulario";
 
 function App() {
+  //list of appointments
+  const [citas, setCitas] = useState([]);
+
+  //add new appointment
+  const createAppointment = (cita) => {
+    setCitas([...citas, cita]);
+  };
+
   return (
     <div className="App">
       <h1>Patient Manager</h1>
       <div className="container">
         <div className="row">
           <div className="one-half column">
-            <Formulario />
+            <Formulario crearCita={createAppointment} />
           </div>
           <div className="one-half column">2</div>
         </div>
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
 
-function Formulario() {
+function Formulario({ crearCita }) {
   // Create appointment state
   const [cita, setCita] = useState({
     mascota: "",
@@ -9,6 +10,9 @@ function Formulario() {
     hora: "",
     sintomas: "",
   });
+  //errors state
+  const [errors, setErrors] = useState(false);
+
   //Update appointment state
   const actualizarCita = (e) => {
     setCita({
@@ -16,11 +20,50 @@ function Formulario() {
       [e.target.name]: e.target.value,
     });
   };
+  //Extract values
+  const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+  //Add appoinment
+
+  const submitCita = (e) => {
+    e.preventDefault();
+
+    //Validate form
+    if (
+      mascota.trim() === "" ||
+      propietario.trim() === "" ||
+      fecha.trim() === "" ||
+      hora.trim() === "" ||
+      sintomas.trim() === ""
+    ) {
+      setErrors(true);
+      return;
+    }
+    //Delete error message in case of previuos error
+    setErrors(false);
+
+    //Assign ID
+    cita.id = uuid();
+
+    //Create appoinment
+    crearCita(cita);
+
+    //Refresh form
+    setCita({
+      mascota: "",
+      propietario: "",
+      fecha: "",
+      hora: "",
+      sintomas: "",
+    });
+  };
+
   return (
     <div>
       <h2>Create appointment</h2>
 
-      <form action="">
+      {errors ? <p className="alerta-error">All fields are required</p> : null}
+      <form action="" onSubmit={submitCita}>
         <label htmlFor="">Pet name</label>
         <input
           type="text"
@@ -28,6 +71,7 @@ function Formulario() {
           className="u-full-width"
           placeholder="Pet name"
           onChange={actualizarCita}
+          value={mascota}
         />
         <label htmlFor="">Owner name</label>
         <input
@@ -36,6 +80,7 @@ function Formulario() {
           className="u-full-width"
           placeholder="Pet owner name"
           onChange={actualizarCita}
+          value={propietario}
         />
         <label htmlFor="">Date</label>
         <input
@@ -43,19 +88,22 @@ function Formulario() {
           name="fecha"
           className="u-full-width"
           onChange={actualizarCita}
+          value={fecha}
         />
         <label htmlFor="">Time</label>
         <input
           type="time"
-          name="fecha"
+          name="hora"
           className="u-full-width"
           onChange={actualizarCita}
+          value={hora}
         />
         <label htmlFor="">Symptoms</label>
         <textarea
           name="sintomas"
           className="u-full-width"
           onChange={actualizarCita}
+          value={sintomas}
         ></textarea>
         <button type="submit" className="u-full-width button-primary">
           Add appointment
